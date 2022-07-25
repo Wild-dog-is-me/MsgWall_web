395e1626872b871f2de89e78b6fca3829686339f
zhanghaojian <1427774041@qq.com>
2022-07-25 15:42:28 +0800

    留言墙note前端开发

---
+++ src/components/NodeCard.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="node-card" :style="{width: width,background:'rgba(252,175,162,0.30)'}">
+    <div class="top">
+      <p class="time">2022.7.25</p>
+      <p class="label">爱情</p>
+    </div>
+    <p class="message">后来那么悲伤，只是因为相遇的时候那么美，如果悲剧不能逆转，那么你在故事开头所体会到的欢乐，最后都要用双倍的悲伤来偿还。</p>
+    <div class="foot">
+      <div class="foot-left">
+        <div class="icon">
+          <span class="iconfont icon-aixin1"></span>
+          <span class="value">3</span>
+        </div>
+        <div class="icon">
+          <span class="iconfont icon-liuyan"></span>
+          <span class="value">3</span>
+        </div>
+      </div>
+      <div class="name">Odin</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "NodeCard",
+  props:{
+    width:{
+      default: '288px'
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+@font-face {
+  font-family: fa;
+  src: url("../assets/fonts/icon/HanziPenSC.ttf");
+}
+
+  .node-card {
+    height: 240px;
+    padding: 10px 20px;
+    box-sizing: border-box;
+    position: relative;
+
+    .top {
+      display: flex;
+      justify-content: space-between;
+
+      p {
+        color: @gray-3;
+        font-size: 14px;
+      }
+    }
+    .message {
+      height: 140px;
+      font-size: 16px;
+      color: @gray-1;
+      font-family: fa;
+      cursor: pointer;
+    }
+
+    .foot {
+      display: flex;
+      justify-content: space-between;
+      position: absolute;
+      left: 0;
+      padding: 0 20px;
+      bottom: 16px;
+      width: 100%;
+      box-sizing: border-box;
+
+      .foot-left {
+        display: flex;
+
+        .value {
+          font-size: @size-12;
+          color: @gray-3;
+          line-height: 16px;
+          padding-left: 4px;
+        }
+
+        .iconfont {
+          font-size: 16px;
+          color: @gray-3;
+        }
+
+        .icon {
+          padding-right: 8px;
+          display: flex;
+          align-items: center;
+        }
+
+        .icon-aixin1 {
+          cursor: pointer;
+          transform: @tr;
+          &:hover {
+            color: @like;
+          }
+        }
+      }
+    }
+        .name {
+          font-family: fa;
+          font-size: 17px;
+          color: @gray-1;
+          font-weight: 600;
+        }
+  }
+
+</style>
--- src/views/WallMessage.vue
+++ src/views/WallMessage.vue
@@ -1,20 +1,88 @@
 <template>
   <div class="wall-message">
-    a
+    <p class="title">{{wallType[id].name}}</p>
+    <P class="slogan">{{wallType[id].slogan}}</P>
+    <div class="label">
+      <p class="label-list" :class="{labelSelected:nlabel == -1}" @click="selectNode(-1)">全部</p>
+      <p class="label-list" :class="{labelSelected:nlabel == index}" v-for="(e,index) in label[id]" :key="index" @click="selectNode(index)">{{e}}</p>
+    </div>
+    <div class="card">
+      <node-card></node-card>
+    </div>
   </div>
 </template>
 
 <script>
+import {wallType,label} from "@/utils/data";
+import nodeCard from "@/components/NodeCard";
+
 export default {
-  name: "WallMessage"
+  name: "WallMessage",
+  data(){
+    return {
+      wallType,
+      label,
+      id: 0, // 留言墙与照片墙的切换id
+      nlabel:-1 // 当前对应标签
+    }
+  },
+  methods:{
+    // 切换标签
+    selectNode(e) {
+      this.nlabel = e;
+    }
+  },
+  components:{
+    nodeCard,
+  }
 }
 </script>
 
 <style scoped>
-  .wall-message{
+  .wall-message {
     /*height: 2000px;*/
-    min-height: 698px;
+    min-height: 650px;
+    padding-top: 52px;
+    /*display: flex;*/
+    /*justify-content: center;*/
+
+  }
+  .title {
+    padding-top: 48px;
+    padding-bottom: 8px;
+    font-size: 56px;
+    color: #202020;
+    text-align: center;
+    font-weight: 600;
+    font-family: FZZJ;
   }
 
+  .slogan {
+    color: #5B5B5B;
+    text-align: center;
+  }
 
+  .label {
+    display: flex;
+    justify-content: center;
+    margin-top: 40px;
+  }
+
+  .label-list {
+    padding: 0 14px;
+    line-height: 28px;
+    margin: 4px;
+    color: #5B5B5B;
+    align-items: center;
+    height: 30px;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
+
+  .labelSelected {
+    color: #202020;
+    font-weight: 600;
+    border: 1px solid #202020;
+    border-radius: 16px;
+  }
 </style>
